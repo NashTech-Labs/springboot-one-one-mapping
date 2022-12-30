21ed1b678e93f47e0c4e3fd99f704fcaa3a5addc
Deepak Kumar <deepak@knoldus.com>
2022-12-30 15:52:05 +0530

    initial commit

---
+++ src/main/java/com/knoldus/springbootoneonemapping/SpringbootOneOneMappingApplication.java
@@ -0,0 +1,13 @@
+package com.knoldus.springbootoneonemapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootOneOneMappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootOneOneMappingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/knoldus/springbootoneonemapping/SpringbootOneOneMappingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knoldus.springbootoneonemapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootOneOneMappingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
